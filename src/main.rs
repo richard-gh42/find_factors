7106bf886488f04cf527fbcd599081726a109aa2
richard-gh42 <richard@ecrcg.de>
2025-09-12 13:05:39 +0200

    Moved functionality out of the main funktion; Added "--help" and "--version" arguments Removed printing out found factor pairs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-use std::{env, sync::mpsc::{self, Receiver}, thread}; 
+use std::{collections::VecDeque, env, sync::mpsc::{self, Receiver}, thread}; 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Find Factors\nversion 1.0\nUse: find_factors [u64]");
+    let mut args: VecDeque<String> = env::args().collect();
+    if args.len() == 1 {
+        print_small_help();
         return;
     };
-    
-    let res = args[1].parse::<u64>();
+
+    let _ = args.pop_front();
+    if args.len() == 1 {
+        if args[0] == "--help".to_string() {
+            print_help();
+        } else if args[0] == "--version".to_string() {
+            println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+        } else {
+            list_factors(args);
+        }
+    } else {
+        print_small_help();
+    }
+}
+
+fn print_small_help() {
+    println!("{} {}\nUse '{} --help' for help", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_NAME"))
+}
+
+fn print_help() {
+    print_small_help();
+    println!("
+ --version -> Prints out name and Version
+")
+}
+
+fn list_factors(mut args: VecDeque<String>) {
+    let res = args.pop_front().unwrap().parse::<u64>();
     let divident: u64;
     match res {
         Ok(_) => divident = res.unwrap(),
@@ -19,20 +45,29 @@ fn main() {
         return;
     }
 
+    let working_factors = factors_of(divident);
+
+    println!();
+    for i in 0..working_factors.len() {
+        println!("{}", working_factors[i])
+    }
+}
+
+pub fn factors_of(num:u64) -> Vec<u64> {
     let mut reces: Vec<Receiver<Vec<u64>>> = Vec::new();
-    let mut working_divisors: Vec<u64> = Vec::new();
+    let mut factors: Vec<u64> = Vec::new();
     let mut threats = Vec::new();
     
     for i in 1..16 {
         let (tran, rece) = mpsc::channel();
         reces.push(rece);
         threats.push(thread::spawn( move || {
-            tran.send(check(i, divident, 15))
+            tran.send(check(i, num, 15))
         }));
     };
 
     for rece in reces {
-        working_divisors.append(&mut rece.recv().unwrap());
+        factors.append(&mut rece.recv().unwrap());
     };
 
     for thr in threats {
@@ -40,28 +75,24 @@ fn main() {
         let _ = join.unwrap();
     };
 
-    working_divisors.sort();
+    factors.sort();
 
-    println!();
-    for i in 0..working_divisors.len() {
-        println!("{}", working_divisors[i])
-    }
+    return factors;
 }
 
-fn check(start: u64, divident: u64, increment: u64) -> Vec<u64> {
-    let mut divisor = start;
-    let mut working_divisors: Vec<u64> = Vec::new();
-    let max_value = divident as f64;
+fn check(start: u64, num: u64, increment: u64) -> Vec<u64> {
+    let mut factor = start;
+    let mut working_factors: Vec<u64> = Vec::new();
+    let max_value = num as f64;
     let max_value = max_value.sqrt() as u64;
-    while divisor <= max_value {
-        if divident%divisor == 0 {
-            println!("{} * {}", divisor, divident/divisor);
-            working_divisors.push(divisor);
-            if divisor != divident/divisor {
-                working_divisors.push(divident/divisor);
+    while factor <= max_value {
+        if num%factor == 0 {
+            working_factors.push(factor);
+            if factor != num/factor {
+                working_factors.push(num/factor);
             }
         }
-        divisor += increment;
+        factor += increment;
     }
-    return working_divisors;
+    return working_factors;
 }
